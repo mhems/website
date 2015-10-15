8ef9e23b75ce09636b4dd1bce105edf80a17e1f3
Matt Hems <mhems1@binghamton.edu>
2015-10-15 08:42:38 -0400

    final junit touches

--- cs140/install_junit.sh
+++ cs140/install_junit.sh
@@ -2,16 +2,16 @@
 
 wget "http://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar"
 wget "http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar"
-echo 'Libraries fetched sucecssfully.'
+echo 'Libraries fetched sucecssfully'
 
-mkdir -p ~/bin ~/lib
+mkdir -p ~/lib
 mv *junit-4.12.jar ~/lib/junit-4.12.jar
 mv *hamcrest-core-1.3.jar ~/lib/hamcrest-core-1.3.jar
 
-echo 'export CLASSPATH=~/lib/junit-4.12.jar:~/lib/hamcrest-core-1.3.jar:$CLASSPATH' >> .bashrc
-echo 'java -cp $CLASSPATH:. org.junit.runner.JUnitCore "$@"' > ~/bin/junit
-chmod u+x ~/bin/junit
+echo 'export CLASSPATH="~/lib/junit-4.12.jar:~/lib/hamcrest-core-1.3.jar:$CLASSPATH"' >> .bashrc
+echo 'alias junit='"'"'java -cp $CLASSPATH:. org.junit.runner.JUnitCore'"'"           >> .bashrc
 echo 'export PATH=$PATH:~/bin' >> .bashrc
-echo '\n\n\n*********************************************'
+echo; echo; echo;
+echo '*********************************************'
 echo 'Environment successfully configured for junit'
 echo '*********************************************'
